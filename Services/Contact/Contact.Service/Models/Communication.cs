9d97b7af06bd668993d83c8648bb7acd2dadd8d7
oyerl <oyerl@DESKTOP-9HU095J>
2022-09-24 17:05:14 +0300

    Add project files.

---
+++ Services/Contact/Contact.Service/Models/Communication.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Contact.Service.Models
+{
+    public class Communication
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        
+        public CommunicationType CommunicationType { get; set; }
+        
+        public string Address { get; set; }
+
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string ContactId { get; set; }
+    }
+
+    public enum CommunicationType
+    {
+        EMAIL,
+        PHONE,
+        LOCATION
+    }
+}
---
+++ Services/Contact/Contact.Service/Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Contact.Service.Models
+{
+    public class Contact
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+       
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+        
+        public string Company { get; set; }
+        
+        [BsonIgnore]
+        public List<Communication> Communications { get; set; }
+    }
+}
