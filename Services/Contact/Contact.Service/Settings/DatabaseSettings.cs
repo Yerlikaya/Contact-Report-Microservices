e2c19de84087d90bd22d56e7d9488f03b5d25b13
oguzhan <o.yerlikaya07@gmail.com>
2022-09-25 02:05:11 +0300

    Added mongodb configurations to contact service

---
+++ Services/Contact/Contact.Service/Settings/DatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Contact.Service.Settings
+{
+    public class DatabaseSettings : IDatabaseSettings
+    {
+        public string ContactCollectionName { get; set; }
+        public string CommunicationCollectionName { get; set; }
+        public string DatabaseName { get; set; }
+        public string ConnectionString { get; set; }
+    }
+}
---
+++ Services/Contact/Contact.Service/Settings/IDatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Contact.Service.Settings
+{
+    public interface IDatabaseSettings
+    {
+        public string ContactCollectionName { get; set; }
+        public string CommunicationCollectionName { get; set; }
+        public string DatabaseName { get; set; }
+        public string ConnectionString { get; set; }
+    }
+}
