ee17d0906bbded24377cca602c71fc9dd242daa2
oguzhan <o.yerlikaya07@gmail.com>
2022-09-25 20:13:41 +0300

    Created Report Service

---
+++ Service/Report/Report.Service/Models/Report.cs
@@ -0,0 +1,15 @@
+﻿namespace Report.Service.Models
+{
+    public class Report
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public ReportStatusType Status { get; set; }
+        public string ReportPath { get; set; }
+    }
+    public enum ReportStatusType
+    {
+        INPROGRESS,
+        COMPLETED
+    }
+}
